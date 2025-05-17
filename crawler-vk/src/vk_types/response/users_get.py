b97dd96b2ce9635e5239bebae9f326e06a1d06ac
Ivan Baklanov <nymless.ib@gmail.com>
2025-05-18 01:11:17 +0500

    Remove unused types

--- crawler-vk/src/vk_types/response/users_get.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-
-from src.vk_types.vk_api_objects.user import User
-
-
-class UsersGet(TypedDict):
-    """https://dev.vk.com/ru/method/users.get"""
-
-    response: list[User]
--- crawler-vk/src/vk_types/vk_api_objects/geo.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-
-from src.vk_types.vk_api_objects.place import Place
-
-
-class Geo(TypedDict):
-    type: str
-    coordinates: str
-    place: Place
